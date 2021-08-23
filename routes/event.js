7ca4b9c89f5072dbca90cce168a74466cc97dd64
cherylteh <cherylteh@gmail.com>
2021-08-24 02:15:46 +0800

    Routing completed for both event and songlib tables

---
+++ routes/event.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var router = express.Router();
+const db = require("../model/helper");
+
+//localhost:5000/event
+
+const getAllEvents = (req, res) => {
+  db("SELECT * FROM event ORDER BY id ASC;")
+    .then(results => res.send(results.data))
+    .catch(err => console.log(err));
+};
+
+/* GET All Events (by Id) - WORKS */
+router.get('/', function(req, res, next) {
+  //res.send('respond with a resource');
+  db("SELECT * FROM event ORDER BY id ASC;")
+    .then(results => {
+      res.send(results.data);
+    })
+    .catch(err => res.status(500).send(err));
+});
+
+// GET one Event by Id - WORKS!
+router.get("/:id", function(req, res, next) {
+    db(`SELECT * FROM event WHERE id=${req.params.id};`)
+      .then(results => {
+       //console.log(results.data); // for checking
+       res.send(results.data[0]);
+      })
+      .catch(err => res.status(404).send("There is no Event with this title"));
+  });
+
+  // ADD new Event - WORKS
+router.post("/", function(req, res, next) {
+    //let newEvent = req.body;
+    db(
+      `INSERT INTO event (eventName,location,date) VALUES("${req.body.eventName}", "${req.body.location}", "${req.body.date}")`
+    )
+      .then(results => {
+        getAllEvents(req, res); // get full list of Songs
+        res.status(201).send("New event added");
+      })
+      .catch(err => res.status(500).send(err));
+  });
+  
+  // Update Existing Event - WORKS
+  router.put("/:id", (req, res, next) => {
+    console.log("Hello");
+    // let newItems = req.body;
+    console.log(`UPDATE event SET 
+    eventName ="${req.body.eventName}", 
+    location ="${req.body.location}" 
+    WHERE id =${req.params.id};`);
+    // console.log(req.params.id); // just to check, not necessary
+    db(`UPDATE event SET 
+        eventName ="${req.body.eventName}", 
+        location ="${req.body.location}",
+        date = ${req.body.date}, 
+        WHERE id =${req.params.id};`)
+      .then(results => {
+        getAllEvents(req, res); // should get back full list of items
+        res.status(201).send("Event Updated");
+      })
+      .catch(err => res.status(500).send(err));
+  });
+  
+  // DELETE an Event by ID - WORKS
+  router.delete("/:id", function(req, res, next) {
+    console.log(req.params);
+    db(`DELETE FROM event WHERE id=${req.params.id}`)
+      .then(results => {
+        getAllEvents(req, res);
+        res.send("Event deleted");
+      })
+      .catch(err => res.status(404).send(err));
+  });
+
+module.exports = router;
---
+++ routes/song.js
@@ -0,0 +1,117 @@
+var express = require('express');
+var router = express.Router();
+const db = require("../model/helper");
+
+const getAllSongs = (req, res) => {
+  db("SELECT * FROM songlib ORDER BY id ASC;")
+    .then(results => res.send(results.data))
+    .catch(err => console.log(err));
+};
+
+//GET All Songs (by Id) - WORKS!
+router.get('/', function(req, res, next) {
+  //res.send('respond with a resource');
+  db("SELECT * FROM songlib ORDER BY id ASC;")
+    .then(results => {
+      res.send(results.data);
+    })
+    .catch(err => res.status(500).send(err));
+});
+
+//GET All Songs (by Composer) - WORKS!
+router.get('/composer', function(req, res, next) {
+  //res.send('respond with a resource');
+  db("SELECT * FROM songlib ORDER BY composer ASC;")
+    .then(results => {
+      res.send(results.data);
+    })
+    .catch(err => res.status(500).send(err));
+});
+
+//GET All Songs (by Title) - WORKS!
+router.get('/title', function(req, res, next) {
+  //res.send('respond with a resource');
+  db("SELECT * FROM songlib ORDER BY title ASC;")
+    .then(results => {
+      res.send(results.data);
+    })
+    .catch(err => res.status(500).send(err));
+});
+
+// GET one Song by Id - WORKS!
+router.get("/:id", function(req, res, next) {
+  db(`SELECT * FROM songlib WHERE id=${req.params.id};`)
+    .then(results => {
+     //console.log(results.data); // for checking
+     res.send(results.data[0]);
+    })
+    .catch(err => res.status(404).send("There is no Song with this title"));
+});
+
+// GET one Song by Title - WORKS
+router.get("/title/:title", function(req, res, next) {
+  db(`SELECT * FROM songlib WHERE title="${req.params.title}";`)
+    .then(results => {
+     console.log(results.data); // for checking
+     res.send(results.data);
+    })
+    .catch(err => res.status(404).send("There is no Song with this title"));
+});
+
+// GET one Song by Composer - WORKS
+router.get("/composer/:composer", function(req, res, next) {
+  db(`SELECT * FROM songlib WHERE composer="${req.params.composer}";`)
+    .then(results => {
+      console.log(results.data); // for checking
+     // res.send(results.data[title]);
+    })
+    .catch(err => res.status(404).send("There is no Song with this composer"));
+});
+
+// ADD new Song - WORKS
+router.post("/", function(req, res, next) {
+  //let newSong = req.body;
+  db(
+    `INSERT INTO songlib (title,composer) VALUES("${req.body.title}", "${req.body.composer}")`
+  )
+    .then(results => {
+      getAllSongs(req, res); // get full list of Songs
+      res.status(201).send("New song added");
+    })
+    .catch(err => res.status(500).send(err));
+});
+
+// Update Existing Song
+router.put("/:id", (req, res, next) => {
+  console.log("Hello");
+  // let newItems = req.body;
+  console.log(`UPDATE songlib SET 
+  title ="${req.body.title}", 
+  composer ="${req.body.composer}" 
+  WHERE id =${req.params.id};`);
+  // console.log(req.params.id); // just to check, not necessary
+  db(`UPDATE songlib SET 
+      title ="${req.body.title}", 
+      composer ="${req.body.composer}" 
+      WHERE id =${req.params.id};`)
+    .then(results => {
+      getAllSongs(req, res); // should get back full list of items
+      res.status(201).send("Updated");
+    })
+    .catch(err => res.status(500).send(err));
+});
+
+// DELETE a student by ID
+router.delete("/:id", function(req, res, next) {
+  console.log(req.params);
+  db(`DELETE FROM songlib WHERE id=${req.params.id}`)
+    .then(results => {
+      getAllSongs(req, res);
+      res.send("Song deleted");
+    })
+    .catch(err => res.status(404).send(err));
+});
+
+
+
+module.exports = router;
